a522bf406263b021948954f95826de7b7c433963
Baqir Naqvi <84667131+Baqir-Naqvi@users.noreply.github.com>
2024-03-04 14:10:43 +0500

    Update index.js

--- src/lib/index.js
+++ src/lib/index.js
@@ -57,7 +57,7 @@ export async function getUserDetails() {
     const session = cookies().get("session")?.value;
     if (!session) return null;
     const parsed = await decrypt(session);
-    const response =await fetch(`http://localhost:3000/api/user?id=${parsed.userExists._id}`)
+   const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}api/user?id=${parsed.userExists._id}`)
     const user=await response.json()
     return user
 }
