a2eecbc0174511ca6ec60842766ae97a1082aea6
Baqir-Naqvi <b.naqvi99@gmail.com>
2024-03-05 11:47:07 +0500

    Refactor verifyToken function and comment out unused code

--- src/app/[lang]/auth/verifyemail/page.jsx
+++ src/app/[lang]/auth/verifyemail/page.jsx
@@ -1,18 +1,39 @@
 import React from 'react'
 import SetPassword from '@/components/auth/SetPassword'
 
+async function verifyToken(token) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+    fetch(
+      `${process.env.NEXT_PUBLIC_BASE_URL}api/auth/verifyemail?token=${token}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        resolve(data);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+    }, 1000);
+  }
+  );
+}
+
 
 async function page({ searchParams }) {
     const token = searchParams.token;
     if (!token) {
         return <div>Missing Token</div>;
     }
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_BASE_URL}api/auth/verifyemail?token=${token}`
-    );
-    const data = await res.json();
-    let email=data.body
-    console.log(data)
+    // const res = await fetch(
+    //   `${process.env.NEXT_PUBLIC_BASE_URL}api/auth/verifyemail?token=${token}`
+    // );
+    // const data = await res.json();
+    // let email=data.body
+    // console.log(data)
+    const data = await verifyToken(token);
+    let email = data.body;
+    
   return (
     <div
       className="flex flex-col items-center justify-center h-screen
