3a9bc747277ad79ea3504c8651515054e99a20fd
Baqir-Naqvi <b.naqvi99@gmail.com>
2024-03-05 11:58:44 +0500

    Debugging verify email on prod

--- src/app/[lang]/auth/verifyemail/page.jsx
+++ src/app/[lang]/auth/verifyemail/page.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import SetPassword from '@/components/auth/SetPassword'
+import Loader from '@/components/shared/Loader';
+
 
 async function verifyToken(token) {
   return new Promise((resolve, reject) => {
@@ -10,9 +12,11 @@ async function verifyToken(token) {
       .then((res) => res.json())
       .then((data) => {
         resolve(data);
+        console.log(data);
       })
       .catch((err) => {
         reject(err);
+        console.log(err);
       });
     }, 1000);
   }
@@ -25,15 +29,10 @@ async function page({ searchParams }) {
     if (!token) {
         return <div>Missing Token</div>;
     }
-    // const res = await fetch(
-    //   `${process.env.NEXT_PUBLIC_BASE_URL}api/auth/verifyemail?token=${token}`
-    // );
-    // const data = await res.json();
-    // let email=data.body
-    // console.log(data)
     const data = await verifyToken(token);
+    console.log("data", data);
     let email = data.body;
-    
+
   return (
     <div
       className="flex flex-col items-center justify-center h-screen
