b692db7ae8b9ce1fdb51620628ddbcf43d9bd699
Baqir-Naqvi <b.naqvi99@gmail.com>
2024-03-05 11:42:18 +0500

    Logs debugging

--- src/app/[lang]/auth/verifyemail/page.jsx
+++ src/app/[lang]/auth/verifyemail/page.jsx
@@ -5,13 +5,14 @@ import SetPassword from '@/components/auth/SetPassword'
 async function page({ searchParams }) {
     const token = searchParams.token;
     if (!token) {
-        return <div>Vaildation failed</div>;
+        return <div>Missing Token</div>;
     }
     const res = await fetch(
       `${process.env.NEXT_PUBLIC_BASE_URL}api/auth/verifyemail?token=${token}`
     );
     const data = await res.json();
     let email=data.body
+    console.log(data)
   return (
     <div
       className="flex flex-col items-center justify-center h-screen
