8d803bdfa0bbfb79b970c22f04132a420790d0ea
Baqir-Naqvi <b.naqvi99@gmail.com>
2024-03-05 12:22:35 +0500

    Move component to client side for validation

--- src/app/[lang]/auth/verifyemail/page.jsx
+++ src/app/[lang]/auth/verifyemail/page.jsx
@@ -1,32 +1,37 @@
 import React from "react";
-import SetPassword from "@/components/auth/SetPassword";
+// import SetPassword from '@/components/auth/SetPassword'
+import dynamic from "next/dynamic";
+import Loader from "@/components/shared/Loader";
+const SetPassword = dynamic(() => import("@/components/auth/SetPassword"), {
+  loading: () => <Loader />,
+  ssr: false,
+});
 
 async function page({ searchParams }) {
   const token = searchParams.token;
   if (!token) {
     return <div>Vaildation failed</div>;
   }
-  try {
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_BASE_URL}api/auth/verifyemail?token=${token}`
-    );
-    const data = await res.json();
-    let email = data.body;
-    return (
-      <div
-        className="flex flex-col items-center justify-center h-screen
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}api/auth/verifyemail?token=${token}`,{
+      cache: "no-cache",
+    }
+  );
+  const data = await res.json();
+  let email = data.body;
+  console.log(data);
+  return (
+    <div
+      className="flex flex-col items-center justify-center h-screen
     bg-cover bg-center bg-no-repeat bg-hero-pattern"
-      >
-        {data.status === 200 ? (
-          <SetPassword email={email} token={token} />
-        ) : (
-          <div>Verification failed</div>
-        )}
-      </div>
-    );
-  } catch (error) {
-    return { data: { status: 400 } };
-  }
+    >
+      {data.status === 200 ? (
+        <SetPassword email={email} token={token} />
+      ) : (
+        <div>Verification failed</div>
+      )}
+    </div>
+  );
 }
 
 export default page;
