ad3847423558de291a7cb9d4125f7d095dbdf87a
Baqir-Naqvi <b.naqvi99@gmail.com>
2024-03-05 12:10:07 +0500

    Refactor verifyemail page component

--- src/app/[lang]/auth/verifyemail/page.jsx
+++ src/app/[lang]/auth/verifyemail/page.jsx
@@ -1,49 +1,32 @@
-import React from 'react'
-import SetPassword from '@/components/auth/SetPassword'
-import Loader from '@/components/shared/Loader';
-
-
-async function verifyToken(token) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-    fetch(
-      `${process.env.NEXT_PUBLIC_BASE_URL}api/auth/verifyemail?token=${token}`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        resolve(data);
-        console.log(data);
-      })
-      .catch((err) => {
-        reject(err);
-        console.log(err);
-      });
-    }, 1000);
-  }
-  );
-}
-
+import React from "react";
+import SetPassword from "@/components/auth/SetPassword";
 
 async function page({ searchParams }) {
-    const token = searchParams.token;
-    if (!token) {
-        return <div>Missing Token</div>;
-    }
-    const data = await verifyToken(token);
-    console.log("data", data);
+  const token = searchParams.token;
+  if (!token) {
+    return <div>Vaildation failed</div>;
+  }
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_URL}api/auth/verifyemail?token=${token}`
+    );
+    const data = await res.json();
     let email = data.body;
-
-  return (
-    <div
-      className="flex flex-col items-center justify-center h-screen
+    return (
+      <div
+        className="flex flex-col items-center justify-center h-screen
     bg-cover bg-center bg-no-repeat bg-hero-pattern"
-    >
-      {data.status === 200 ?
-      <SetPassword email={email} token={token} />
-      : <div>Verification failed</div>}
-
-    </div>
-  );
+      >
+        {data.status === 200 ? (
+          <SetPassword email={email} token={token} />
+        ) : (
+          <div>Verification failed</div>
+        )}
+      </div>
+    );
+  } catch (error) {
+    return { data: { status: 400 } };
+  }
 }
 
-export default page
+export default page;
