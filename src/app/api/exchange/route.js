13989c7448d3388e59fbe7e412a57bbb8966b8ec
Baqir-Naqvi <b.naqvi99@gmail.com>
2024-03-08 03:13:33 +0500

    Add currency conversion response in GET method

--- src/app/api/exchange/route.js
+++ src/app/api/exchange/route.js
@@ -3,7 +3,7 @@
 
 export async function GET() {
     try {
-     
+        return Response.json({ status: 200, message: 'Currency conversion fetched' });
     } catch (error) {
         console.error('Error fetching currency conversion:', error);
         return Response.json({ status: 500, message: 'Unable to fetch currency conversion' });
