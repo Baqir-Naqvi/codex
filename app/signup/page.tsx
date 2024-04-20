73f4c280b14deed9cbb6557075bb2e59c10b83e9
Muhammad Fahimul Huq <167473888+FarzanaHuq@users.noreply.github.com>
2024-04-20 15:09:14 +0600

    feat: updated signup page

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -9,18 +9,26 @@
 
 import AcmeLogo from '@/app/ui/acme-logo';
 import SignUpForm from '@/app/ui/signup-form';
+import mascout from '@/public/logo/mascot2.png'
+import Image from 'next/image';
 
 export default function SignUpPage() {
   return (
-    <main className="flex items-center justify-center md:h-screen">
-      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
-        <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
-          <div className="w-32 text-white md:w-36">
-            <AcmeLogo />
-          </div>
+    <main className="flex justify-between h-screen">
+      <div className='basis-1/2'>
+        <div className='flex flex-col justify-between h-screen'>
+        <p className='text-3xl font-bold mt-[75px] ml-[39px]'>signstory</p>
+        <p className='ml-[39px] text-6xl max-w-[616px]'>Welcome to <span className='text-[#389FCF] font-bold'>signstory</span>, where stories come to life!</p>
+        <Image className='self-center' src={mascout} alt='signstory-logo'/>
         </div>
+ 
+      </div>
+      <div className='bg-[#CBE7F4] pt-[87px] pl-[43px] basis-1/2 mt-[75px] rounded-l-xl'>
+        
+        <p className='text-4xl font-extrabold'>Create Account</p>
         <SignUpForm />
       </div>
+        
     </main>
   );
 }
--- app/ui/signup-form.tsx
+++ app/ui/signup-form.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { ChangeEvent, FormEvent, useState } from 'react';
 
 export default function SignUpForm() {
@@ -10,13 +11,25 @@ export default function SignUpForm() {
     parentPhoneNumber: '',
     preferredLanguage: '',
   });
+const [focusRing, setFocusRing] = useState(false)
+const [showPassword, setShowPassword] = useState(false);
+
+
+const handleTogglePassword = () => {
+  setShowPassword((prevShowPassword) => !prevShowPassword);
+};
 
   const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+   
     const { name, value } = e.target;
     setFormData((prevData) => ({
       ...prevData,
       [name]: value,
     }));
+
+    if(e.nativeEvent.type !== 'input'){
+      setFocusRing(true);
+    }
   };
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
@@ -36,9 +49,11 @@ export default function SignUpForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="space-y-4">
+    <>
+    
+    <form onSubmit={handleSubmit} className="mt-5 space-y-4 flex flex-col w-[655px]">
       <div>
-        <label htmlFor="email">Email</label>
+       
         <input
           type="email"
           id="email"
@@ -46,21 +61,38 @@ export default function SignUpForm() {
           value={formData.email}
           onChange={handleChange}
           required
+          placeholder='Email Address'
+          className='border-none rounded-xl w-[655px] p-4'
+
         />
       </div>
-      <div>
-        <label htmlFor="password">Password</label>
+      <div className='relative'>
+      
         <input
-          type="password"
+        type={showPassword ? 'text' : 'password'}
           id="password"
           name="password"
           value={formData.password}
           onChange={handleChange}
           required
+          placeholder='Password'
+          className='border-none rounded-xl w-[655px] p-4'
         />
+      <div className='absolute top-3 right-4'>
+      {!showPassword ? (<svg xmlns="http://www.w3.org/2000/svg" onClick={handleTogglePassword} fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+  <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+</svg>
+): ( <svg xmlns="http://www.w3.org/2000/svg" onClick={handleTogglePassword} fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+<path strokeLinecap="round" strokeLinejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
+</svg>
+)}
+      </div>
+
+
       </div>
       <div>
-        <label htmlFor="childName">Child&apos;sName</label>
+     
         <input
           type="text"
           id="childName"
@@ -68,10 +100,12 @@ export default function SignUpForm() {
           value={formData.childName}
           onChange={handleChange}
           required
+          placeholder='Child Name'
+          className='border-none rounded-xl w-[655px] p-4'
         />
       </div>
       <div>
-        <label htmlFor="age">Child&apos;s Age</label>
+   
         <input
           type="number"
           id="age"
@@ -79,10 +113,12 @@ export default function SignUpForm() {
           value={formData.age}
           onChange={handleChange}
           required
+          placeholder='Child Age'
+          className='border-none rounded-xl w-[655px] p-4'
         />
       </div>
       <div>
-        <label htmlFor="parentName">Parent&apos;s Name</label>
+     
         <input
           type="text"
           id="parentName"
@@ -90,10 +126,12 @@ export default function SignUpForm() {
           value={formData.parentName}
           onChange={handleChange}
           required
+          placeholder='Parents Name'
+          className='border-none rounded-xl w-[655px] p-4'
         />
       </div>
       <div>
-        <label htmlFor="parentPhoneNumber">Parent&apos;s Phone Number</label>
+       
         <input
           type="tel"
           id="parentPhoneNumber"
@@ -101,9 +139,12 @@ export default function SignUpForm() {
           value={formData.parentPhoneNumber}
           onChange={handleChange}
           required
+          placeholder='Parents Phone Number'
+          className='border-none rounded-xl w-[655px] p-4'
         />
       </div>
-      <div>
+      <div className={`${focusRing ? `focus:ring-1` : ''} bg-white flex flex-col w-[655px] h-[95px] py-3 px-4 rounded-xl`}
+       >
         <label htmlFor="preferredLanguage">Preferred Language</label>
         <select
           id="preferredLanguage"
@@ -111,6 +152,7 @@ export default function SignUpForm() {
           value={formData.preferredLanguage}
           onChange={handleChange}
           required
+          className='text-gray-400 border-none mt-2 border-transparent focus:border-transparent focus:ring-0'
         >
           <option value="">Select Language</option>
           <option value="English">English</option>
@@ -118,7 +160,12 @@ export default function SignUpForm() {
           <option value="Other">Other</option>
         </select>
       </div>
-      <button type="submit">Sign Up</button>
+      <div className='self-center'>
+      <button type="submit" className='py-4 px-[140px] bg-[#E6864E] text-white font-bold text-3xl rounded-[70px]'>Sign Up</button>
+      </div>
+
     </form>
+    <p className='mt-4 text-lg text-gray-500'>Already have an account ? <Link href="/login" className='font-bold text-black'>Log In</Link> </p>
+    </>
   );
 }
